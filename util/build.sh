f4223653b12a1d9f7a30b524629a15a565f9fe2f
Muh Muhten <muh.muhten@gmail.com>
2016-10-19 06:50:46 -0400

    reduce extra work when building

--- util/build.sh
+++ util/build.sh
@@ -6,16 +6,11 @@ export DESTDIR="$PWD/1"
 # copy overlay
 cp -a overlay/ "$DESTDIR"
 
-# apply patches
-sh util/preimage.sh 0 "$DESTDIR" b
-diff -ru a b | patch -sp 1 -d "$DESTDIR"
-find "$DESTDIR" -name '*.orig' -delete
-
 # build skaware
 build_ska() {
 	cd "skarnet/$1"; shift
-	./configure "$@"
-	gmake it strip install
+	[ -f config.mak ] || ./configure "$@"
+	gmake it install LDFLAGS=-s
 }
 (build_ska skalibs --disable-shared)
 (build_ska execline \
@@ -27,3 +22,12 @@ build_ska() {
 	--with-include="$DESTDIR/usr/include" \
 	--with-lib="$DESTDIR/usr/lib/skalibs" \
 	--with-lib="$DESTDIR/usr/lib/execline")
+
+# preimage from a first so we can build without 0/, but note that we need to
+# rebuild before installing
+sh util/preimage.sh a "$DESTDIR" b
+sh util/preimage.sh 0 "$DESTDIR" b
+
+# apply patches
+diff -ru a b | patch -sp 1 -d "$DESTDIR"
+find "$DESTDIR" -name '*.orig' -delete
