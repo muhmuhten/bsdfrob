3692eea64afc193c12fd6898ea73bd3392cbdf5d
Muh Muhten <muh.muhten@gmail.com>
2016-11-11 21:27:58 -0500

    improve configurability for host differences

--- util/install.sh
+++ util/install.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 set -eu
 
-if [ ${1+1} ]; then
-	ty=$1
-elif [ -x 0/boot/kernel/kernel ]; then
-	ty=host
-elif [ "`sysctl -n security.jail.jailed`" = 1 ]; then
-	ty=jail
+if read quirks < /media/host/quirks; then
+	set host $quirks
+else
+	set jail
 fi
 
-tar -cf - --numeric-owner --uid=0 --gid 0 1 -C "$ty" install @mtree | \
+tar -cf - --numeric-owner --uid=0 --gid 0 1 | \
 	tar -xvf - --strip-components 1 -C 0
+
+for ea; do
+	tar -cf - --numeric-owner --uid=0 --gid 0 -C "quirks/$ea" files @mtree | \
+		tar -xvf - --strip-components 1 -C 0
+done
