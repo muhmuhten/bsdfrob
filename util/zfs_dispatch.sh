a748e6cde883a93219cc4e800874c4c0dfe6329a
Muh Muhten <muh.muhten@gmail.com>
2016-11-21 20:38:33 -0500

    script patch updates

--- util/zfs_dispatch.sh
+++ util/zfs_dispatch.sh
@@ -10,7 +10,19 @@ case $2 in
 	export pool="$1/i" quirks="jail"
 	shift 2; exec sh util/zfs_rebuild.sh "$@"
 	;;
+(patch)
+	case $3 in
+	(host) export pool="$1/0" ;;
+	(jail) export pool="$1/i" ;;
+	(*)
+		echo "$0: not sure what to do about '$2 $3'" >&2
+		exit 2
+		;;
+	esac
+	shift 3; exec sh util/zfs_patch.sh "$@"
+	;;
 (*)
 	echo "$0: not sure what to do about '$2'" >&2
 	exit 2
+	;;
 esac
