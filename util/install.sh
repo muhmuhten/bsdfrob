e29d877a36460e0055d5482646f53eb4ef033cf1
Muh Muhten <muh.muhten@gmail.com>
2017-08-05 17:00:15 -0400

    disentangle dhclient from serial console
    Serial console appears not to be standard across datacenters, and causes
    boot to fail if not present.

--- util/install.sh
+++ util/install.sh
@@ -11,6 +11,7 @@ tar -cf - --numeric-owner --uid=0 --gid 0 "$build" | \
 	tar -xvf - --strip-components 1 -C "$DESTDIR"
 
 for ea; do
+	[ -d "quirks/$ea" ] || continue
 	tar -cf - --numeric-owner --uid=0 --gid 0 -C "quirks/$ea" files @mtree | \
 		tar -xvf - --strip-components 1 -C "$DESTDIR"
 done
