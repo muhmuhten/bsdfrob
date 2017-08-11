a636bea57853c3f51414977861f6d612990b36fc
Muh Muhten <muh.muhten@gmail.com>
2017-08-11 17:07:54 -0400

    reduce forking in build

--- util/build.sh
+++ util/build.sh
@@ -11,25 +11,24 @@ build_ska() {
 	cd "skarnet/$1"; shift
 	[ -f config.mak ] || ./configure "$@"
 	gmake it install LDFLAGS=-s DESTDIR="$build"
+	cd ../..
 }
-(build_ska skalibs --disable-shared)
-(build_ska execline \
+build_ska skalibs --disable-shared
+build_ska execline \
 	--with-sysdeps=../skalibs/sysdeps.cfg \
 	--with-include="$build/usr/include" \
-	--with-lib="$build/usr/lib/skalibs")
-(build_ska s6 \
+	--with-lib="$build/usr/lib/skalibs"
+build_ska s6 \
 	--with-sysdeps=../skalibs/sysdeps.cfg \
 	--with-include="$build/usr/include" \
 	--with-lib="$build/usr/lib/skalibs" \
-	--with-lib="$build/usr/lib/execline")
+	--with-lib="$build/usr/lib/execline"
 
-build_prog() {
-	cd "progs/$1"
-	make "$1"
-	cp -a "$1" "$build/$2"
-}
-(build_prog jset sbin)
-(build_prog jat sbin)
-(build_prog jrm sbin)
-(build_prog setlogin sbin)
-(build_prog subreap bin)
+make -C progs/jset jset jat jrm
+make -C progs/setlogin setlogin
+make -C progs/subreap subreap
+cp -l \
+	progs/jset/jset progs/jset/jat progs/jset/jrm \
+	progs/setlogin/setlogin \
+	progs/subreap/subreap \
+	"$build/sbin"
